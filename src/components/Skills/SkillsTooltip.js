2959d2389c85c3dcc64dce690fb851fe43512054
suzyng83209 <suzyng.one@gmail.com>
2017-09-06 22:39:37 -0400

    adding antd to the site

--- src/components/Skills/SkillsTooltip.js
+++ src/components/Skills/SkillsTooltip.js
@@ -1,30 +1,30 @@
 import React from "react";
 import styled from "styled-components";
-import { Glyphicon, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Tooltip, Icon } from "antd";
 
 import "./SkillsTooltip.css";
 
 const IconWrapper = styled.div`
-    margin: -20px 8px 0;
-    display: flex;
-    align-items: flex-start;
-    span {
+  margin: -20px 8px 0;
+  display: flex;
+  align-items: flex-start;
+  span {
     font-size: 20px;
-    }
+  }
 `;
 
-const tooltip = (
-  <Tooltip id="tooltip" bsClass="Skills tooltip">
+const tooltipText = (
+  <div>
     <div style={{ marginTop: "4px", fontSize: "16px" }}>Why piecharts?</div>
     <p>
-      Rather than using an arbitrary ranking system for the languages I use,
-      I compare them in terms of my ability to use them relative to each other.
+      Rather than using an arbitrary ranking system for the languages I use, I
+      compare them in terms of my ability to use them relative to each other.
     </p>
-  </Tooltip>
+  </div>
 );
 
 export default () => (
-  <OverlayTrigger placement="right" overlay={tooltip}>
-    <IconWrapper><Glyphicon glyph="question-sign" /></IconWrapper>
-  </OverlayTrigger>
+  <Tooltip placement="right" title={tooltipText}>
+    <Icon type="question-circle" />
+  </Tooltip>
 );
